d3350a53c1b246a948c2353d8edb3962d2aa1380
Shubham Pareek <shubhampareek4000@gmail.com>
2021-10-01 19:01:17 -0700

    thread confusion

--- src/main/java/cs601/Project2/FileTransfer.java
+++ src/main/java/cs601/Project2/FileTransfer.java
@@ -40,18 +40,19 @@ public class FileTransfer {
             int finalI = i;
 
 //            This implementation is not working
-//            Thread t1 = new Thread(() -> publisher1.publish(txt.get(finalI)));
-//            Thread t2 = new Thread(() -> publisher2.publish(txt1.get(finalI)));
-//            t1.start();
-//            t2.start();
-//
-//            t2.run();
-//            t1.run();
+            Thread t1 = new Thread(() -> publisher1.publish(txt.get(finalI)));
+            Thread t2 = new Thread(() -> publisher2.publish(txt1.get(finalI)));
+            t1.start();
+            t2.start();
+
+            t2.run();
+            t1.run();
+
 //            This implmentation is working
-            tPool.submit( () -> {
-               publisher1.publish(txt.get(finalI));
-               publisher2.publish(txt1.get(finalI));
-            });
+//            tPool.submit( () -> {
+//               publisher1.publish(txt.get(finalI));
+//               publisher2.publish(txt1.get(finalI));
+//            });
 
         }
 
