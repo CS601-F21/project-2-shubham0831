d94a6695e94eb7fa24c99913927cdd1feaa9e433
Shubham Pareek <shubhampareek4000@gmail.com>
2021-10-02 03:50:02 -0700

    started work on async broker, have the idea, yet to implement

--- src/main/java/cs601/PubSub/Brokers/AsyncUnorderedDispatchBroker.java
+++ src/main/java/cs601/PubSub/Brokers/AsyncUnorderedDispatchBroker.java
@@ -2,16 +2,25 @@ package cs601.PubSub.Brokers;
 
 import cs601.PubSub.Subscriber.Subscriber;
 
+import java.util.HashSet;
+
 
 public class AsyncUnorderedDispatchBroker <T> implements Broker <T>{
+    private HashSet<Subscriber> subList = new HashSet<>();
     @Override
     public void publish(T item) {
-
+        Thread t1 = new Thread(
+                () -> {
+                    for (Subscriber sub : subList){
+                        sub.onEvent(item);
+                    }
+                }
+        );
     }
 
     @Override
-    public void subscribe(Subscriber subscriber) {
-
+    public synchronized void subscribe(Subscriber subscriber) {
+        subList.add(subscriber);
     }
 
     @Override
